9166dace23b507f75b834fd97c04a4c209973fb5
彭浚翔 <cxz0917001997@gmail.com>
2021-02-24 17:30:21 +0800

    test api

--- src/app/@feature/task/components/task-manage-page/task-manage-page.component.ts
+++ src/app/@feature/task/components/task-manage-page/task-manage-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ITaskListItem, TaskService} from '@core/services/task.service';
 
 @Component({
   selector: 'task-manage-page',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-manage-page.component.scss']
 })
 export class TaskManagePageComponent implements OnInit {
-
-  constructor() { }
+  columnList = [
+    {
+      key: 'TaskName',
+      name: '名稱'
+    },
+    {
+      key: 'TaskStatus',
+      name: '狀態'
+    },
+    {
+      key: 'TaskType',
+      name: '類別'
+    },
+    {
+      key: 'TaskDetail',
+      name: '說明'
+    },
+    {
+      key: 'CreateTime',
+      name: '新增時間'
+    },
+    {
+      key: 'UpdateTime',
+      name: '更新時間'
+    }
+  ];
+  taskList: ITaskListItem[] = [];
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   ngOnInit(): void {
+    this.getTaskList();
   }
 
+  getTaskList() {
+    this.taskService.getTaskList().subscribe(res => {
+      this.taskList = res.data;
+    });
+  }
 }
